cd0beb6d377c56acb58e862b3ed59e414f97efc5
littlepuppi <emilyjolam@gmail.com>
2025-01-06 11:22:58 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+var input = `tUrpEntine and turtlEs`
+console.log(input.length)
+
+let phrase = ''
+
+// start at 0  
+for(let i=0; i<input.length; i++){
+ console.log(i)
+ console.log(input[i])
+ // is this a vowel?
+  if(input[i] === 'a'){
+    phrase += 'A'
+  }
+  if(input[i] === 'e'){
+    phrase += 'EE'
+  }
+  if(input[i] === 'u'){
+    phrase += 'UU'
+  }
+  if(input[i] === 'i'){
+    phrase += 'I'
+  }
+  if(input[i] === 'o'){
+    phrase += 'O'
+  }
+}
+
+console.log(phrase);
